b22bf2ef9e2054647f9cfb994fb644aefdb6e739
Suuuuuzy <yujianjiasuzy@gmail.com>
2024-04-19 19:37:25 -0400

    fix taint log output for d8

--- src/taint_tracking/taint_tracking.cc
+++ src/taint_tracking/taint_tracking.cc
@@ -357,7 +357,6 @@ class LogTaintTask : public v8::Task {
   LogTaintTask(Isolate* isolate) : isolate_(isolate) {}
 
   void Run() override {
-    std::cout << "jianjia see LogTaintTask" << std::endl;
     TaintTracker::FromIsolate(isolate_)->Get()->DoFlushLog();
   }
 
@@ -672,15 +671,12 @@ int64_t TaintTracker::Impl::LogToFileImpl(
   log_message.setMessageId(msg_id);
 
   if (buffered_log_) {
-    std::cout << "jianjia check buffered_log_" << std::endl;
     std::lock_guard<std::mutex> guard(log_mutex_);
     builder.DoSynchronousWrite(*buffered_log_);
   }
 
-  if (unsent_messages_ > kFlushMessageMax ||
-      conf == FORCE_FLUSH ||
+  if (unsent_messages_ > kFlushMessageMax || conf == FORCE_FLUSH ||
       last_message_flushed_.HasExpired(kMaxTimeBetweenFlushes)) {
-    std::cout << "jianjia check FORCE_FLUSH" << std::endl;
     ScheduleFlushLog(isolate);
     last_message_flushed_.Restart();
   } else {
